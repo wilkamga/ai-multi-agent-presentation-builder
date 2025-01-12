e9ddf3f521054df55cfc11fbda5b3b11f110b9cc
Luiz Braz <luiz.braz@microsoft.com>
2025-01-12 13:06:52 -0300

    General adjustments on the presentation layout

--- src/plugins/presentation.py
+++ src/plugins/presentation.py
@@ -9,23 +9,38 @@ class PresentationPlugin:
     @kernel_function(description="Create a presentation deck in a PDF format.")
     def create_presentation(self, 
                             title: Annotated[str, "The title of the presentation"],
+                            subtitle: Annotated[str, "The subtitle of the presentation"],
                             content: Annotated[str, "The content of the decks"]) -> Annotated[str, "Create a presentation."]:
 
         # Create a presentation object
         prs = Presentation(pptx='green.pptx')
 
+        # Remove title and subtitle from the slides content
+        content = content.replace(title, '')
+        content = content.replace(subtitle, '')
+
         # Split the content into slides
-        slides_content = content.split("\n\n")
-
+        slides_content = content.split("#")
+        initial_slide = True
+        
         for slide_content in slides_content:
-            # Add a slide with the specified layout
-            slide_layout = prs.slide_layouts[0]
-            slide = prs.slides.add_slide(slide_layout)
-
-            # Add title and content to the slide
-            title, *body = slide_content.split("\n")
-            slide.shapes.title.text = title
-            slide.placeholders[1].text = "\n".join(body)
+            # The first slide must have a title and subtitle
+            if initial_slide:
+                slide_layout = prs.slide_layouts[0]
+                slide = prs.slides.add_slide(slide_layout)
+                slide.shapes.title.text = title
+                slide.placeholders[1].text = subtitle
+                initial_slide = False
+                continue
+            
+            if len(slide_content) > 0:
+                title = slide_content.split('\n')[0]
+                slide_content = slide_content.replace(title, '').strip()
+                # Add the content for the other slides
+                slide_layout = prs.slide_layouts[1]
+                slide = prs.slides.add_slide(slide_layout)
+                slide.shapes.title.text = title
+                slide.placeholders[1].text = slide_content
 
         # Save the presentation
         output_path = "presentation.pptx"
